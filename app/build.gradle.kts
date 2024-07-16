d7c00e8e41e41d3af818b31ca640c66bffa24334
Rafael Moraes <rafaelmoraes005@gmail.com>
2024-07-16 16:12:40 -0300

    Pratica 3 - Parte 2: Removendo elementos dinamicamente usando ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
